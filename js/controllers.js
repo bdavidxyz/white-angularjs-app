1cfed278cc00b8d0703cee7a3d07dff3ee94785e
david <david@crunchbang.(none)>
2013-07-04 11:41:51 +0200

    first commit

---
+++ js/controllers.js
@@ -0,0 +1,12 @@
+'use strict';
+
+myApp.controller("MyCtrl1" ,function ($scope, UtilSrvc) {
+    $scope.aVariable = 'anExampleValueWithinScope';
+    $scope.valueFromService = UtilSrvc.helloWorld("Amy");
+});
+
+myApp.controller("MyCtrl2" ,function ($scope) {
+
+});
+
+// you may add more controllers below
